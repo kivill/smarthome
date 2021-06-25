209df07df53a6518014115a7d1b59e6200a9de12
Kirill Pozdeev <kivillsaw146@gmail.com>
2021-06-25 15:06:37 +0500

    fix doc

--- api/src/triggers/jobs.controller.ts
+++ api/src/triggers/jobs.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { TriggersService } from './triggers.service';
-
+import {
+    ApiCreatedResponse,
+    ApiOkResponse,
+    ApiTags,
+    ApiBearerAuth,
+    ApiHeader,
+    ApiOperation,
+    ApiParam
+} from '@nestjs/swagger';
 import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('job')
+@ApiTags('Job')
 export class JobController {
   constructor(private readonly triggersService: TriggersService) {}
   @Get()
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Список заданий', })
+  @ApiCreatedResponse({})
   findAll() {
     return this.triggersService.findJobs();
   }
